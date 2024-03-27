17baddfbd2e2c16325b138478e28d12e053c96be
dalia eltohami <daliaeltohami1991@gmail.com>
2024-03-27 23:11:41 +0200

    product-redux

--- client/src/store/reducers/productReducer.js
+++ client/src/store/reducers/productReducer.js
@@ -1,5 +1,12 @@
+import { FETCH_PRODUCTS } from "../actions/types";
+
 const productReducer = (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    case FETCH_PRODUCTS:
+      return { products: action.data };
+    default:
+      return state;
+  }
 };
 
 export default productReducer;
