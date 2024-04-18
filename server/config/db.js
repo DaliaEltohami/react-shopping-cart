b049f5f20cf1dde5346eae7de9c837ecf2b45d1a
dalia eltohami <daliaeltohami1991@gmail.com>
2024-04-18 03:51:59 +0200

    heroku deployment

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const DB = () => {
   const connectionString = "mongodb://127.0.0.1:27017/react-shopping-cart";
   mongoose
-    .connect(connectionString)
+    .connect(process.env.MONGO_URI || connectionString)
     .then(() => {
       console.log("Database Connected Successfully! ");
     })
--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const product = require("./models/productModel");
 const productRouter = require("./routes/routes");
 const orderRouter = require("./routes/orderRoutes");
-const DB = require("./config/DB");
+const DB = require("./config/db");
 
 const app = express();
+
 app.use(bodyParser.json());
+DB();
 app.use("/", productRouter);
 app.use("/", orderRouter);
 
-app.listen(3005, (req, res) => {
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("public"));
+  app.get("/", (req, res) => {
+    res.send(__dirname + "/public/index.html");
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API Running");
+  });
+}
+
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, (req, res) => {
   console.log("Server Running on Port 3005");
 });
-
-DB();
